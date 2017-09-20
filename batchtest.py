e8efe5bed6ee7993a3ca175f92088d5ff147bfc3
liyun dai <dlyun2009@gmail.com>
2017-09-20 10:39:45 +0800

    ok

--- batchtest.py
+++ batchtest.py
@@ -2,15 +2,17 @@ import os
 from os import listdir
 import datetime
 import subprocess
-
+import commands
 
 for f in listdir("cases"):
     print(f)
     cmd = 'AND.exe cases/'+f
     print( cmd)
     now=datetime.datetime.now()
-    p=subprocess.Popen([cmd],stdout=subprocess.PIPE)
-    out=p.stdout.read()
+    status, output = commands.getstatusoutput(cmd)
+    print(output)
+    # p=subprocess.Popen([cmd],stdout=subprocess.PIPE)
+    # out=p.stdout.read()
     # os.system(cmd)
     print (datetime.datetime.now()-now)
 
