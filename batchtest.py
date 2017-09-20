c85ce3835a0abf0f33dde0832b88aed290394669
fanzhetalent <32116676+fanzhetalent@users.noreply.github.com>
2017-09-20 10:56:51 +0800

    Revert "ok"
    This reverts commit 0f9e0945924eef30d7dfef8af86aa26b4c830153.

--- batchtest.py
+++ batchtest.py
@@ -1,19 +1,22 @@
 import os
 from os import listdir
 import datetime
+
 import subprocess
-import commands
+
+
 
 for f in listdir("cases"):
     print(f)
     cmd = 'AND.exe cases/'+f
     print( cmd)
     now=datetime.datetime.now()
-    status, output = commands.getstatusoutput(cmd)
-    print(output)
-    # p=subprocess.Popen([cmd],stdout=subprocess.PIPE)
+
+    r=subprocess.check_output(cmd,shell=False)
     # out=p.stdout.read()
-    # os.system(cmd)
+    print ("hhhhhhhhhhhhhhhhhh"+str(r))
+    os.system(cmd)
+
     print (datetime.datetime.now()-now)
 
     
