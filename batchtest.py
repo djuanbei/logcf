503172473e9185209d8418eb8b937fafa4d86fea
liyun dai <dlyun2009@gmail.com>
2017-09-20 11:06:04 +0800

    add compute real root function

--- batchtest.py
+++ batchtest.py
@@ -5,19 +5,20 @@ import sys
 
 import subprocess
 
-
-
 for f in listdir("cases"):
-    fsock = open('error.log', 'w')
-    sys.stderr = fsock  
+
     cmd = 'AND.exe cases/'+f
     print( cmd)
     now=datetime.datetime.now()
 
     r=subprocess.check_output(cmd,shell=False)
-    # out=p.stdout.read()
-    print ("hhhhhhhhhhhhhhhhhh"+str(r))
-    os.system(cmd)
+    r=str(r)
+    realrootnum=len([c for c in r if r=='['])
+    if realrootnum <1 :
+        realrootnum=0
+    else
+        realrootnum-=1
+    print("real root number is: "+realrootnum)
 
     print (datetime.datetime.now()-now)
 
